08cfbfd75328a33c4976ae35f5085a76399c4ab3
John Gebbie <me@johngebbie.com>
2022-10-29 19:56:26 +0100

    install: make suitable for build systems

--- install-numen.sh
+++ install-numen.sh
@@ -1,38 +1,42 @@
 #!/bin/sh
+# ./install-numen.sh [DESTDIR] [BINDIR]
+#
+# If the environment variable PACKAGING is set, compiling speech.go is left
+# to you to do beforehand. It is meant for build systems packaging numen.
+# Example: PACKAGING=true ./install-numen.sh "$DESTDIR" /usr/bin
 
-ok=1
-! command -v arecord > /dev/null && echo 'you need the alsa-utils package' && unset ok
-! command -v dmenu > /dev/null && echo 'you need dmenu' && unset ok
-! command -v gcc > /dev/null && echo 'you need gcc' && unset ok
-! command -v go > /dev/null && echo 'you need go (sometimes packaged as golang)' && unset ok
-! command -v scdoc > /dev/null && echo 'you need scdoc' && unset ok
-! command -v xdotool > /dev/null && echo 'you need xdotool' && unset ok
-[ "$ok" ] || exit
+if ! [ "$PACKAGING" ]; then
+	ok=1
+	! command -v arecord > /dev/null && echo 'you need the alsa-utils package' && unset ok
+	! command -v dmenu > /dev/null && echo 'you need dmenu' && unset ok
+	! command -v gcc > /dev/null && echo 'you need gcc' && unset ok
+	! command -v go > /dev/null && echo 'you need go (sometimes packaged as golang)' && unset ok
+	! command -v scdoc > /dev/null && echo 'you need scdoc' && unset ok
+	! command -v xdotool > /dev/null && echo 'you need xdotool' && unset ok
+	[ "$ok" ] || exit
 
-# Install the numen command
-rm -rf /usr/libexec/numen && mkdir -p /usr/libexec/numen || exit
-go build speech.go || exit
-for f in *; do
-	if [ -f "$f" ] && [ -x "$f" ]; then
-		cp "$f" "/usr/libexec/numen/$f" || exit
-	fi
-done
-cp -r handlers /usr/libexec/numen/handlers || exit
-cat > /usr/local/bin/numen << 'EOF'
-#!/bin/sh
-cd /usr/libexec/numen
-NUMEN_MODEL="${NUMEN_MODEL:-/usr/local/share/vosk-models/small-en-us}" exec ./numen "$@"
-EOF
-chmod +x /usr/local/bin/numen || exit
+	go build speech.go || exit
+
+	# not necessary but lets you run ./numen in this directory
+	ln -sf /usr/share/vosk-models/small-en-us model
+fi
+
+# Install executables for internal use
+libexec="$1/usr/libexec/numen"
+rm -rf "$libexec" && mkdir -p "$libexec" || exit
+cp awk instructor numen record scribe speech "$libexec" || exit
+cp -r handlers "$libexec" || exit
+
+# Install commands
+bin="$1/${2:-/usr/local/bin}"
+mkdir -p "$bin" || exit
+cp wrapper "$bin/numen" || exit
+# Install commands used in the default phrases
+cp displaying "$bin" || exit
 
 # Install the default phrases
 mkdir -p /etc/numen && rm -rf /etc/numen/phrases && cp -r phrases /etc/numen || exit
-# Install the displaying command used in the default phrases
-cp displaying /usr/local/bin || exit
 # Install the manpage
-scdoc < doc/numen.1.scd > /usr/share/man/man1/numen.1
-
-# not necessary but lets you run ./numen in this directory
-ln -sf /usr/local/share/vosk-models/small-en-us model
+scdoc < doc/numen.1.scd > /usr/share/man/man1/numen.1 || exit
 
 echo 'Installed successfully.'
