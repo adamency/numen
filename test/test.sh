cfa30a5d1318b0e71902ea5c39e19618a2723d3d
John Gebbie <me@johngebbie.com>
2023-06-01 18:06:06 +0100

    test: use a dummy dotool

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# add a dummy dotool to $PATH so this doesn't require /dev/uinput
+export PATH="$PWD/dummy:$PATH"
+
 if test "$(NUMEN_PIPE=/tmp/numen-test-pipe ../numen --audio=test.wav test.phrases)" = \
 'n
 u
